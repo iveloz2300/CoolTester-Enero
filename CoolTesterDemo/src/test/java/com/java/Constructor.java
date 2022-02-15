388f8d0e526f883058750e7b92f9e0099875edf1
iveloz <Pichicha16>
2022-02-14 21:05:19 -0500

    class demo

---
+++ CoolTesterDemo/src/test/java/com/java/Constructor.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class Constructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	//Perro perro2 = new Perro("San Bernardo", "Blanco", "Grande", "Cool",3);
+
+}
